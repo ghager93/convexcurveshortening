d9e32571b6003805f5ec2165058f360aead3136a
Gerard Hager <ghager93@gmail.com>
2021-07-29 10:44:23 +1000

    Commented

--- enclosed_csf_list.py
+++ enclosed_csf_list.py
@@ -5,12 +5,22 @@ import csf_list
 
 
 def enclosed_csf_list(curve: np.ndarray, n_subsets: int, step_size: float = 1):
+    """
+    Runs the enclosed curve shortening flow algorithm on a curve and returns n_subsets curves that are have an enclosed
+    area linearly-spaced between the initial curves enclosed area and zero.
+
+    :param curve: Nx2 Numpy array of the outline of an image.  Curve must run clockwise.
+    :param n_subsets: Number of subsets to return. Subsets are linearly proportional of the area of the initial outline.
+    :param step_size: Step size for each iteration of the concave ECSF algorithm.
+    If the algorithm fails, try a smaller step size.
+    :return:  n_subsets long list of 2D numpy arrays.
+    """
     ecsf_obj = concave_enclosed_csf_list.ConcaveEnclosedCSFList(curve, step_size=step_size)
 
     try:
         ecsf_obj.run()
-    except Exception as e:
-        raise e
+    except Exception as curve_loop_detected:
+        raise curve_loop_detected
 
     num_concave_curves = max(1, int((1 - ecsf_obj.last_to_first_curve_area_ratio()) * n_subsets))
 
@@ -24,9 +34,19 @@ def enclosed_csf_list(curve: np.ndarray, n_subsets: int, step_size: float = 1):
 
 
 def enclosed_csf_list_retry_on_fail(curve: np.ndarray, n_subsets: int, step_size: float = 1):
+    """
+    Runs enclosed_csf_list(). If algorithm fails, step_size is reduced by a factor of 5 and the algorithm is run again.
+    Fails if algorithm fails 4 times.
+
+    :param curve: Nx2 Numpy array of the outline of an image.  Curve must run clockwise.
+    :param n_subsets: Number of subsets to return. Subsets are linearly proportional of the area of the initial outline.
+    :param step_size: Step size for each iteration of the concave ECSF algorithm.
+    If the algorithm fails, try a smaller step size.
+    :return:  n_subsets long list of 2D numpy arrays.
+    """
     for attempt in range(4):
         try:
-            ecsf_list = enclosed_csf_list(curve, n_subsets, step_size)
+            ecsf_list =  enclosed_csf_list(curve, n_subsets, step_size)
         except:
             step_size /= 5
         else:
@@ -34,3 +54,4 @@ def enclosed_csf_list_retry_on_fail(curve: np.ndarray, n_subsets: int, step_size
     else:
         raise Exception(f"Loop detected with step size {step_size}. Curve cannot be shortened.")
 
+    return ecsf_list
